7e1f0739d285163fbb6c7b9178c961cc3be59955
CXWorks <1043806046@qq.com>
2015-10-23 23:38:41 +0800

    建工程

---
+++ project/DeliverySystem/Connection/src/main/java/org/Connection/App.java
@@ -0,0 +1,13 @@
+package org.Connection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
