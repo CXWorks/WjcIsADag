94146c60dfba41a5bb3f0484b5136b9dfc54ae52
CXWorks <1043806046@qq.com>
2015-10-25 00:39:42 +0800

    部分接口

--- project/DeliverySystem/Connection/src/main/java/rmi/memberdata/memberDataService.java
+++ project/DeliverySystem/Connection/src/main/java/rmi/memberdata/memberDataService.java
@@ -2,10 +2,43 @@ package rmi.memberdata;
 
 import java.util.ArrayList;
 
+import message.OperationMessage;
 import po.memberdata.StaffPO;
 import po.memberdata.StaffTypeEnum;
-
+/**
+ * 
+ * @author cxworks
+ * 2015/10/24
+ */
 public interface memberDataService {
+	/**
+	 * 获取所有员工信息
+	 * @param staffTypeEnum 员工类型
+	 * @return 员工信息
+	 */
 	public ArrayList<StaffPO> getStaff(StaffTypeEnum staffTypeEnum);
-
+	/**
+	 * 修改员工信息
+	 * @param after 修改后的员工信息
+	 * @return 返回操作结果
+	 */
+	public OperationMessage modifyStaff(StaffPO after);
+	/**
+	 * 新雇佣员工的信息
+	 * @param staff 新的员工信息
+	 * @return
+	 */
+	public OperationMessage addStaff(StaffPO staff);
+	/**
+	 * 解雇员工
+	 * @param staff
+	 * @return
+	 */
+	public OperationMessage dismissStaff(StaffPO staff);
+	/**
+	 * 查询员工
+	 * @param staff 是待查询员工的部分信息
+	 * @return
+	 */
+	public StaffPO searchStaff(StaffPO staff);
 }
