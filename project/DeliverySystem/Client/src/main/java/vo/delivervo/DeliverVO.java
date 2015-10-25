719d234c5e9d91fcd299d52930486a442b735679
Carter1996 <wangjc19960926@163.com>
2015-10-25 10:23:14 +0800

    我的VO

---
+++ project/DeliverySystem/Client/src/main/java/vo/delivervo/DeliverVO.java
@@ -0,0 +1,13 @@
+package vo.delivervo;
+
+/**
+ * 
+ * @author wjc
+ *2015/10/24
+ */
+
+public class DeliverVO {
+	private String orderID;
+	private String data;
+	private String postman;
+}
---
+++ project/DeliverySystem/Client/src/main/java/vo/receivevo/ReceiveVO.java
@@ -0,0 +1,17 @@
+package vo.receivevo;
+
+import po.receivedata.StateEnum;
+
+/**
+ * 
+ * @author wjc
+ *2015/10/24
+ */
+
+public class ReceiveVO {
+	private String orderID;
+	private String transitID;
+	private String data;
+	private String depature;
+	private StateEnum state;
+}
