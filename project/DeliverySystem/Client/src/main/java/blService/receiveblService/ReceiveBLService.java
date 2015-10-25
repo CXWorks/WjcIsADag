1c747be09281a176cf9eb5bb94cf9d6119d50509
Carter1996 <wangjc19960926@163.com>
2015-10-25 10:45:01 +0800

    bl（报错因为mx的orderVO和transitVO还没写）

---
+++ project/DeliverySystem/Client/src/main/java/blService/receiveblService/ReceiveBLService.java
@@ -0,0 +1,49 @@
+package blService.receiveblService;
+
+import java.util.ArrayList;
+
+import message.CheckFormMessage;
+import message.OperationMessage;
+import po.deliverdata.DeliverPO;
+import po.receivedata.ReceivePO;
+
+public interface ReceiveBLService {
+	/**
+	 * 检查到达单
+	 * @param form 到达单信息
+	 * @return 返回检查结果列表
+	 */
+	public ArrayList<CheckFormMessage> checkFormat(ReceivePO form);
+	
+	/**
+	 * 提交到达单
+	 * @param form 到达单信息
+	 * @return 返回操作结果
+	 */
+	public OperationMessage submit(ReceivePO form);
+	
+	/**
+	 * 保存到达单草稿
+	 * @return 返回操作结果
+	 */
+	public OperationMessage saveDraft();
+	
+	/**
+	 * 载入到达单草稿
+	 * @return 到达单信息
+	 */
+	public ReceivePO loadDraft();
+	
+	/**
+	 * 载入订单信息
+	 * @param orderID 订单号
+	 * @return 订单信息
+	 */
+	public OrderVO getOrderVO(String orderID);
+	
+	/**
+	 * 载入中转单信息（已经载入订单信息）
+	 * @return 中转单信息
+	 */
+	public TransitVO getTransitVO();
+}
