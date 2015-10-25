7442ca97044228dc5a72242965d5345fe5d0ee16
Carter1996 <wangjc19960926@163.com>
2015-10-25 11:11:25 +0800

    修改bl

--- project/DeliverySystem/Client/src/main/java/blService/deliverblService/DeliverBLService.java
+++ project/DeliverySystem/Client/src/main/java/blService/deliverblService/DeliverBLService.java
@@ -1,48 +1,48 @@
-package blService.deliverblService;
-
-import java.util.ArrayList;
-
-import message.CheckFormMessage;
-import message.OperationMessage;
-import po.deliverdata.DeliverPO;
-
-/**
- * 
- * @author wjc
- *2015/10/24
- */
-
-public interface DeliverBLService {
-	/**
-	 * 检查派件单
-	 * @param form 派件信息
-	 * @return 返回检查结果列表
-	 */
-	public ArrayList<CheckFormMessage> checkFormat(DeliverPO form);
-	
-	/**
-	 * 提交派件单
-	 * @param form 派件信息
-	 * @return 返回操作结果
-	 */
-	public OperationMessage submit(DeliverPO form);
-	
-	/**
-	 * 保存派件单草稿
-	 * @return 返回操作结果
-	 */
-	public OperationMessage saveDraft();
-	
-	/**
-	 * 载入派件单草稿
-	 * @return 派件单信息
-	 */
-	public DeliverPO loadDraft();
-	
-	/**
-	 * 载入订单信息
-	 * @param orderID 订单号
-	 * @return 订单信息
-	 */
-	public OrderVO getOrderVO(String orderID);
-}
+//package blService.deliverblService;
+//
+//import java.util.ArrayList;
+//
+//import message.CheckFormMessage;
+//import message.OperationMessage;
+//import po.deliverdata.DeliverPO;
+//
+///**
+// * 
+// * @author wjc
+// *2015/10/24
+// */
+//
+//public interface DeliverBLService {
+//	/**
+//	 * 检查派件单
+//	 * @param form 派件信息
+//	 * @return 返回检查结果列表
+//	 */
+//	public ArrayList<CheckFormMessage> checkFormat(DeliverPO form);
+//	
+//	/**
+//	 * 提交派件单
+//	 * @param form 派件信息
+//	 * @return 返回操作结果
+//	 */
+//	public OperationMessage submit(DeliverPO form);
+//	
+//	/**
+//	 * 保存派件单草稿
+//	 * @return 返回操作结果
+//	 */
+//	public OperationMessage saveDraft();
+//	
+//	/**
+//	 * 载入派件单草稿
+//	 * @return 派件单信息
+//	 */
+//	public DeliverPO loadDraft();
+//	
+//	/**
+//	 * 载入订单信息
+//	 * @param orderID 订单号
+//	 * @return 订单信息
+//	 */
+//	public OrderVO getOrderVO(String orderID);
+//}
