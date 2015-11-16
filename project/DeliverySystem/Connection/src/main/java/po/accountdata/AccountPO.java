251013d4d17352233abe292909e6d155e8556db7
CXWorks <1043806046@qq.com>
2015-11-16 19:43:15 +0800

    我负责的文档

--- project/DeliverySystem/Connection/src/main/java/po/accountdata/AccountPO.java
+++ project/DeliverySystem/Connection/src/main/java/po/accountdata/AccountPO.java
@@ -10,6 +10,7 @@ public class AccountPO extends CommonPO implements Serializable{
 	private String ID;
 	private String password;
 	private AuthorityEnum authority;
+	public AccountPO(){}
 
 	public AccountPO(String ID, AuthorityEnum authority, String password) {
 		this.ID = ID;
