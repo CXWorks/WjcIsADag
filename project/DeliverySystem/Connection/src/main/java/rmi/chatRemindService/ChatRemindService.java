0878a099da380382bbf6e39015fa84a819e24c24
CXWorks <1043806046@qq.com>
2015-11-18 00:17:56 +0800

    。。

---
+++ project/DeliverySystem/Connection/src/main/java/rmi/chatRemindService/ChatRemindService.java
@@ -0,0 +1,41 @@
+package rmi.chatRemindService;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import message.ChatMessage;
+import message.OperationMessage;
+
+/**
+ * 
+ * @author wjc
+ *2015/10/24
+ */
+
+public interface ChatRemindService extends Remote{
+
+	/** 接口的名称，RMI绑定时候的名称 */
+	public static final String NAME = "ChatRemind";
+	
+	/**
+	 * 检查数据接口是否存在未读的系统通知
+	 * @param ID 员工的ID
+	 * @return 返回操作结果
+	 */
+	public OperationMessage checkMessage(String ID) throws RemoteException;
+	
+	/**
+	 * 传输消息
+	 * @param mes 传送的消息, ID 员工的ID
+	 * @return 返回操作结果
+	 */
+	public OperationMessage transfer(ArrayList<ChatMessage>mes,String ID) throws RemoteException;
+	
+	/**
+	 * 接收信息
+	 * @param ID 员工的ID
+	 * @return 返回消息列表
+	 */
+	public ArrayList<ChatMessage> receive(String ID) throws RemoteException;
+}
