6c3f8002b05332d37e6184f443c58a31efbf803a
CXWorks <1043806046@qq.com>
2015-11-18 00:07:18 +0800

    for jenkins

---
+++ project/DeliverySystem/Connection/src/test/java/rmi/accountdata/TestDemo.java
@@ -0,0 +1,27 @@
+package rmi.accountdata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+ * Connection//rmi.accountdata//Test.java
+ * @author CXWorks
+ * @date 2015年11月18日 上午12:03:33
+ * @version 1.0 
+ */
+public class TestDemo {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
