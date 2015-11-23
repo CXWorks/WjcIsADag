ebe29c227d3cdce310e2dc22e52012e5e89b22fc
CXWorks <1043806046@qq.com>
2015-11-23 22:46:27 +0800

    UserInfo

---
+++ project/DeliverySystem/Client/src/main/java/userinfo/UserInfo.java
@@ -0,0 +1,40 @@
+package userinfo;
+
+import po.memberdata.StaffTypeEnum;
+
+/** 
+ * Client//userinfo//UserInfo.java
+ * @author CXWorks
+ * @date 2015年11月23日 下午10:40:37
+ * @version 1.0 
+ */
+public class UserInfo {
+	private static String userID;
+	private static StaffTypeEnum staffType;
+	private static String institutionID;
+	//
+	private static boolean logined=false;
+	public static String getUserID() {
+		if (logined) {
+			return userID;
+		} else {
+			return null;
+		}
+	}
+	public static StaffTypeEnum getStaffType() {
+		if (logined) {
+			return staffType;
+		} else {
+			return null;
+		}
+	}
+	public static String getInstitutionID() {
+		if (logined) {
+			return institutionID;
+		} else {
+			return null;
+		}
+	}
+	
+	
+}
