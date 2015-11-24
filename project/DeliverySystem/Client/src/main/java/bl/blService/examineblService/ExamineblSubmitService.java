a8a3da5f7b0242b5becf040b432140faf7ce2d41
CXWorks <1043806046@qq.com>
2015-11-24 21:46:12 +0800

    deliver和examine

--- project/DeliverySystem/Client/src/main/java/bl/blService/examineblService/ExamineblSubmitService.java
+++
@@ -1,8 +0,0 @@
-package bl.blService.examineblService;
-
-import vo.FormVO;
-import message.OperationMessage;
-
-public interface ExamineblSubmitService {
-	public OperationMessage submit(FormVO form);
-}
