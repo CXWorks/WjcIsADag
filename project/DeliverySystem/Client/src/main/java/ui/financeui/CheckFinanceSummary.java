a1ac8f5130f4568118d1d93ce29a58d1e55f0ed9
Sissel-Wu <chengbutang@163.com>
2015-11-24 12:26:14 +0800

    查看营收的界面

---
+++ project/DeliverySystem/Client/src/main/java/ui/financeui/CheckFinanceSummary.java
@@ -0,0 +1,19 @@
+package ui.financeui;
+
+import javafx.scene.control.*;
+
+/**
+ * Created by Sissel on 2015/11/24.
+ */
+public class CheckFinanceSummary {
+    public Button search_Btn;
+    public DatePicker start_DatePicker;
+    public DatePicker end_DatePicker;
+    public Label payment_Label;
+    public Label revenue_Label;
+    public Label profit_Label;
+    public TableView finance_TableView;
+    public TableColumn date_Column;
+    public TableColumn type_Column;
+    public TableColumn money_Column;
+}
