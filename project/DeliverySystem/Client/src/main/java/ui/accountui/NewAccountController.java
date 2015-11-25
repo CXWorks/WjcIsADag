8a521d209cc658abd1ba060c24932630acfb383c
Sissel-Wu <chengbutang@163.com>
2015-11-25 18:30:18 +0800

    找回了之前误删的，删除了MainFrame

--- project/DeliverySystem/Client/src/main/java/ui/accountui/NewAccountController.java
+++ project/DeliverySystem/Client/src/main/java/ui/accountui/NewAccountController.java
@@ -1,24 +1,63 @@
 package ui.accountui;
 
+import bl.blService.accountblService.AccountBLManageService;
+import factory.AccountFactory;
+import factory.BLFactory;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import po.memberdata.StaffTypeEnum;
+import tool.ui.Enum2ObservableList;
+import tool.ui.SimpleEnumProperty;
 import vo.accountvo.AccountVO;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
 public class NewAccountController {
 
 	public TextField id_Field;
-	public TextField pw_Field;
-	public TextField pw_Again_Field;
-	public ChoiceBox<String> type_Box;
-	
-	
-	
-	
-	
-	
-	
-	public void creat(AccountVO avo){
-		
+	public ChoiceBox<SimpleEnumProperty<StaffTypeEnum>> type_ChoiceBox;
+	public TextField password_Field;
+
+	private StaffTypeEnum staffTypeEnum;
+
+	private AccountBLManageService accountBLManageService = AccountFactory.getManageService();
+
+	public static Parent launch() throws IOException {
+		return FXMLLoader.load(NewAccountController.class.getResource("newAccount.fxml"));
 	}
-	
+
+	@FXML
+	public void initialize(){
+		type_ChoiceBox.setItems(
+				Enum2ObservableList.transit(StaffTypeEnum.values())
+		);
+
+		type_ChoiceBox.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> {
+					staffTypeEnum = newValue.getEnum();
+					System.out.println("selected "+staffTypeEnum);
+				}
+		);
+
+		clear(null);
+	}
+
+	public void saveDraft(ActionEvent actionEvent) {
+	}
+
+	public void clear(ActionEvent actionEvent) {
+		id_Field.clear();
+		password_Field.clear();
+		type_ChoiceBox.setValue(new SimpleEnumProperty<>(StaffTypeEnum.DELIVER));
+		staffTypeEnum = StaffTypeEnum.DELIVER;
+	}
+
+	public void sure(ActionEvent actionEvent) {
+	}
 }
