aa1de453c58fdade3fbe471bb01387f76b2c6296
CXWorks <1043806046@qq.com>
2015-12-01 17:38:25 +0800

    万事开头难

--- project/DeliverySystem/Client/src/main/java/ui/manangeui/salary/ManageSalaryController.java
+++ project/DeliverySystem/Client/src/main/java/ui/manangeui/salary/ManageSalaryController.java
@@ -7,26 +7,37 @@ package ui.manangeui.salary;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Observable;
+
+import javax.swing.table.TableCellEditor;
 
 import po.InfoEnum;
 import vo.configurationvo.ConfigurationVO;
+import vo.configurationvo.SalaryStrategyVO;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
 import bl.blService.configurationblService.ConfigurationBLService;
 import factory.ConfigurationFactory;
 
-/**
-* Client//ui.manangeui.salary//ManageSalaryController.java
-* @author CXWorks
-* @date 2015年11月30日 下午7:52:21
-* @version 1.0
-*/
+
 public class ManageSalaryController {
 	public Button closeButton;
 	public Button modifyButton;
 	public Button cancelButton;
+	public TextField staff_type;
+	public TextField base;
+	public TextField commission;
+	public TextField bonus;
+	public TabPane tabPane;
+	private ArrayList<SalaryStrategyVO> vo;
+	private ObservableList<Tab> tabs;
 	//
 	private ConfigurationBLService configurationBLService=ConfigurationFactory.getConfigurationBLService();
 	//
@@ -39,7 +50,17 @@ public class ManageSalaryController {
 	//
 	@FXML
 	public void initialize(){
-		ArrayList<ConfigurationVO> vo=configurationBLService.get(InfoEnum.SALARY);
+		//
+		tabs=tabPane.getTabs();
+		//
+		this.seletedChange();
+		//
+//		ArrayList<ConfigurationVO> vo=configurationBLService.get(InfoEnum.SALARY);
+//		this.vo=new ArrayList<SalaryStrategyVO>(vo.size());
+//		for (ConfigurationVO configurationVO : vo) {
+//			this.vo.add((SalaryStrategyVO)configurationVO);
+//		}
+		//
 		
 	}
 	//
@@ -49,6 +70,20 @@ public class ManageSalaryController {
 	}
 	@FXML
 	private void sure(){
+		
+	}
+	//
+	@FXML
+	private void seletedChange(){
+		tabs=tabPane.getTabs();
+		for (Tab tab : tabs) {
+			if (tab.isSelected()) {
+				staff_type.setText(tab.getText());
+				base.setText("1000");
+				commission.setText("20");
+				bonus.setText("1500");
+			}
+		}
 		
 	}
 }
