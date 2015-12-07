dbcede134cf009724aa6a7149c5479e4d0aa5004
CXWorks <1043806046@qq.com>
2015-12-07 22:58:58 +0800

    城市距离

--- project/DeliverySystem/Connection/src/main/java/rmi/deliverdata/DeliverDataService.java
+++ project/DeliverySystem/Connection/src/main/java/rmi/deliverdata/DeliverDataService.java
@@ -28,7 +28,7 @@ public interface DeliverDataService extends CommonDataService<DeliverPO>{
 	/**
 	 * 按派件员查看派件清单
 	 * @param 快递员ID
-	 * @return 可以被派送的派件清单(订单号列表)
+	 * @return 可以被派送的派件清单(派件单号列表)
 	 */
 	public ArrayList<String> searchAsPerson(String ID) throws RemoteException;
 	
