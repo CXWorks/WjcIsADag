302c422384b09c8542d10091e6e1c75497316666
CCharlesMeng <944438034@qq.com>
2015-12-09 22:51:15 +0800

    就这样自然发生了

--- project/DeliverySystem/Client/src/main/java/ui/accountui/personAccountViewEditDialogController.java
+++ project/DeliverySystem/Client/src/main/java/ui/accountui/personAccountViewEditDialogController.java
@@ -21,27 +21,27 @@ public class personAccountViewEditDialogController {
 	public PasswordField originalPassWord_Field;
 	public PasswordField newPassWord_Field;
 	public PasswordField againNewPassWord_Field;
-	
+
 	public Label attention_Label;
-	
+
 	private AccountVO editVO =  new AccountVO();
-    public Stage stage;
-	
+	public Stage stage;
+
 	public static personAccountViewEditDialogController newDialog(AccountVO editVO) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(personAccountViewEditDialogController.class.getResource("personAccountViewEditDialog.fxml"));
-        Pane pane = loader.load();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(personAccountViewEditDialogController.class.getResource("personAccountViewEditDialog.fxml"));
+		Pane pane = loader.load();
 
-        Stage stage = new Stage();
-        stage.initOwner(Main.primaryStage);
-        stage.setScene(new Scene(pane));
+		Stage stage = new Stage();
+		stage.initOwner(Main.primaryStage);
+		stage.setScene(new Scene(pane));
 
-        personAccountViewEditDialogController controller = (personAccountViewEditDialogController)loader.getController();
-        controller.editVO = editVO;
-        controller.stage = stage;
-        
-        return controller;
-    }
+		personAccountViewEditDialogController controller = (personAccountViewEditDialogController)loader.getController();
+		controller.editVO = editVO;
+		controller.stage = stage;
+
+		return controller;
+	}
 
 	public boolean check(String pw0,String pw1,String pw2){
 		if(pw1==pw2&&pw0==editVO.password){
@@ -58,27 +58,29 @@ public class personAccountViewEditDialogController {
 			againNewPassWord_Field.clear();
 			attention_Label.setText("请输入正确的原始密码！");
 			return false;
-					
-		}
-		
-	}
-	
-	
-    public void ok(ActionEvent actionEvent) {
-        if(check(originalPassWord_Field.getText(),newPassWord_Field.getText(),againNewPassWord_Field.getText())){
-        editVO.password = newPassWord_Field.getText();
-        
-        System.out.println("modify password successfully!");
-        
-        stage.close();}else{
-        	System.out.println("modify password failed!");
-        }
-    }
 
-    public void cancel(ActionEvent actionEvent) {
-        stage.close();
-    }
-	
-	
-	
+		}
+
+	}
+
+
+	public void ok(ActionEvent actionEvent) {
+		if(check(originalPassWord_Field.getText(),
+				newPassWord_Field.getText(),againNewPassWord_Field.getText())){
+			editVO.password = newPassWord_Field.getText();
+
+			System.out.println("modify password successfully!");
+
+			stage.close();
+		}else{
+			System.out.println("modify password failed!");
+		}
+	}
+
+	public void cancel(ActionEvent actionEvent) {
+		stage.close();
+	}
+
+
+
 }
