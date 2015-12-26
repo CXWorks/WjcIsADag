ca25586c1ada8d3050206df8101904ded407ec09
Sissel-Wu <chengbutang@163.com>
2015-12-26 23:17:54 +0800

    物流信息可以测试了

--- project/DeliverySystem/Client/src/main/java/ui/navigationui/DeliverNavigation.java
+++ project/DeliverySystem/Client/src/main/java/ui/navigationui/DeliverNavigation.java
@@ -8,6 +8,7 @@ import ui.deliverui.deliverController;
 import ui.messageui.CheckMessageController;
 import ui.orderui.NewOrderController;
 import ui.orderui.PeopleReceiveFormController;
+import ui.searchui.CheckLogisticsController;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -20,6 +21,7 @@ public class DeliverNavigation {
         Parent node = TabMaker.newLeftTabPane(Arrays.asList(
                 new Pair<String, Parent>("填写收件单", PeopleReceiveFormController.launch()),
                 new Pair<String, Parent>("新建订单", NewOrderController.launch()),
+                new Pair<String, Parent>("物流信息查询", CheckLogisticsController.launch()),
                 new Pair<String, Parent>("个人信息", PersonalAccountViewController.launch()),
                 new Pair<String, Parent>("消息通知", CheckMessageController.launch())
         ));
